e5a1f60d952d010fb662c6eed8db64bdce43029d
Mbarreto24 <mofaba@live.com>
2024-08-29 18:39:33 -0500

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -35,6 +35,52 @@ resource "aws_route_table" "rt_default_proyecto" {
         Name = "rt_default_proyecto"
     }
  }
+resource "aws_security_group" "sg_ssh" {
+  name        = "SG Acceso SSH"
+  description = "Permite el acceso SSH desde cualquier lugar"
+  vpc_id      = aws_vpc.VPC_proyecto.id
+
+  tags = {
+    Name = "SG_acceso_SSH_proyecto"
+  }
+
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/00"]
+    description = "Permite acceso al puerto 22"
+
+  }
+
+ ingress {
+    from_port = 2357
+    to_port = 2357
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/00"]
+    description = "Permite acceso al puerto 22"
+
+  }
+
+ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/00"]
+    description = "Permite acceso al puerto 22"
+
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    description = "Permite salida a cualquier ip y puerto "
+  }  
+}
+
+
 
 resource "aws_route_table_association" "a_rt_default_proyecto" {
    subnet_id = aws_subnet.Subnet_public.id
